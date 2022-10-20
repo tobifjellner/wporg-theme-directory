97c08b5696461f88d91eaf25513d7b112bdf87a0
Kelly Dwan <ryelle@users.noreply.github.com>
2022-10-20 12:05:55 -0400

    Add initial theme files

--- source/wp-content/themes/wporg-themes-2022/functions.php
+++ source/wp-content/themes/wporg-themes-2022/functions.php
@@ -1,8 +1,33 @@
 <?php
 
-add_action( 'init', function() {
-	// Temporary fix for permission problem during local install
+namespace WordPressdotorg\Theme\Theme_Directory_2022;
+
+/**
+ * Actions and filters.
+ */
+add_action( 'init', __NAMESPACE__ . '\fix_term_imports' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+
+/**
+ * Temporary fix for permission problem during local install.
+ */
+function fix_term_imports() {
 	if ( defined( 'WP_CLI' ) ) {
 		remove_filter( 'pre_insert_term', 'wporg_themes_pre_insert_term' );
 	}
-} );
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function enqueue_assets() {
+	// The parent style is registered as `wporg-parent-2021-style`, and will be loaded unless
+	// explicitly unregistered. We can load any child-theme overrides by declaring the parent
+	// stylesheet as a dependency.
+	wp_enqueue_style(
+		'wporg-theme-directory-2022-style',
+		get_stylesheet_uri(),
+		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
+		filemtime( __DIR__ . '/style.css' )
+	);
+}
